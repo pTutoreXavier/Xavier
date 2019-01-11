1bd1f92743b88c0a6219ddb4892912fe65131eab
Louis Maillard <l.maillard55000@gmail.com>
2019-01-11 11:53:35 +0100

    affichage et ajout dictionnaire

--- app/controllers/dictionaryController.php
+++ app/controllers/dictionaryController.php
@@ -4,12 +4,28 @@ use \Slim\Views\Twig as View;
 use \App\Models\Dictionary;
 class DictionaryController extends Controller{
 	public function index($request, $response){
-		$methods = Dictionary::where("type", "methode")->get();
-		$objects = Dictionary::where("type", "objet")->get();
-		return $this->view->render($response, "dictionary.twig", array("methods" => $methods, "objects" => $objects));
+		$methods = Dictionary::where("type", "method")->get();
+		$objects = Dictionary::where("type", "object")->get();
+		return $this->view->render($response, "dictionary/dictionary.twig", array("methods" => $methods, "objects" => $objects));
 	}
 
-	public function getById($request, $response){
-		
+	public function getById($request, $response, $args){
+		$element = Dictionary::find($args["id"]);
+		return $this->view->render($response, "dictionary/details.twig", array("element" => $element));
+	}
+
+	public function new($request, $response, $args){
+		return $this->view->render($response, "dictionary/new.twig", array("type" => $args["type"]));
+	}
+
+	public function create($request, $response, $args){
+		$element = new Dictionary();
+		$element->type = $args["type"];
+		$element->libelle = $request->getParam("name");
+		if($args["type"] == "method"){
+			$element->parametre = $request->getParam("parameter");
+		}
+		$element->save();
+		return $response->withRedirect($this->router->pathFor('dictionary'));
 	}
 }
--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,7 @@
 <?php
 $app->get("/home", "HomeController:index");
 $app->get("/auth/signup", "AuthController:getSignUp")->setName("auth.signup");
-$app->get("/dictionary", "DictionaryController:index");
+$app->get("/dictionary", "DictionaryController:index")->setName("dictionary");
 $app->get("/dictionary/{id}", "DictionaryController:getById");
+$app->get("/dictionary/new/{type}", "DictionaryController:new");
+$app->post("/dictionary/new/{type}/send", "DictionaryController:create");
