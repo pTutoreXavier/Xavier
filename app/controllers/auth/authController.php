9c29d7083d2540c5ac4adeedead6e917aaf5f40b
CrysisWar <adriwarch@hotmail.fr>
2019-01-11 11:03:53 +0100

    Validation du formulaire d'inscription, messages d'erreurs et persistance des données de formulaire

--- app/controllers/auth/authController.php
+++ app/controllers/auth/authController.php
@@ -4,14 +4,29 @@ namespace App\Controllers\Auth;
 
 use \App\Controllers\Controller;
 use \App\Models\User;
+use \Respect\Validation\Validator as v;
 
 class AuthController extends Controller{
-	public function getSignUp($request, $response){
+
+    public function getSignUp($request, $response){
 		return $this->view->render($response, "auth/signup.twig");
 	}
 
 	public function postSignUp($request, $response)
     {
+
+        $validation = $this->validator->validate($request, [
+            'nom' => v::notEmpty()->alpha(),
+            'prenom' => v::noWhitespace()->notEmpty()->alpha(),
+            'mail' => v::noWhitespace()->notEmpty()->email(),
+            'mdp' => v::noWhitespace()->notEmpty(),
+            'mdpConf' => v::equals('mdp'),
+        ]);
+
+        if($validation->failed()){
+            return $response->withRedirect($this->router->pathFor('auth.signup'));
+        }
+
         $user = User::create([
             'nom' => $request->getParam('nom'),
             'prenom' => $request->getParam('prenom'),
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,7 +20,9 @@ $app = new \Slim\App([
 		]
 	]
 ]);
+
 $container = $app->getContainer();
+
 $capsule = new \Illuminate\Database\Capsule\Manager;
 $capsule->addConnection($container["settings"]["db"]);
 $capsule->setAsGlobal();
@@ -28,6 +30,7 @@ $capsule->bootEloquent();
 $container["db"] = function($container) use ($capsule){
 	return $capsule;
 };
+
 $container["view"] = function($container){
 	$view = new \Slim\Views\Twig(__DIR__."/../ressources/views", [
 		"cache" => false,
@@ -38,10 +41,24 @@ $container["view"] = function($container){
 	));
 	return $view;
 };
+
+$container['validator'] = function($container){
+    return new \App\validation\Validator;
+};
+
+
+// CONTROLLERS
 $container["HomeController"] = function($container){
 	return new \App\Controllers\HomeController($container);
 };
 $container["AuthController"] = function($container){
 	return new \App\Controllers\Auth\AuthController($container);
 };
+
+// MIDDLEWARES
+$app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
+$app->add(new \App\Middleware\OldInputMiddleware($container));
+
+
+
 require __DIR__."/../app/routes.php";
