acdfe8fb8f0d10496d1265603cc207050fb8d499
Guebel <marc.guebel@gmx.fr>
2019-02-28 12:57:47 +0100

    Fin updatPass + updateMail

--- app/controllers/profilController.php
+++ app/controllers/profilController.php
@@ -10,40 +10,83 @@ class ProfilController extends Controller{
 			"nom" => $user->nom,
 			"prenom" => $user->prenom,
 			"mail" => $user->mail,
+			"dateNaissance" => $user->dateNaissance,
 			"mdp" => $user->mdp,
 			"level" => $user->level));
 	}
 	public function updatePass($request, $response){
 		$user = User::find(1);/* A remplacer par l'id  dans la session*/
-		if ($response) {
-			var_dump($request);
+		if (isset($_SESSION['errorPass'])) {
+			echo $_SESSION['errorPass'];
 		}
+		session_destroy();
 		return $this->view->render($response, 'profil/profilUpdatePass.twig', array(
 			"id" => $user->id, 
 			"nom" => $user->nom,
 			"prenom" => $user->prenom,
 			"mail" => $user->mail,
+			"dateNaissance" => $user->dateNaissance,
 			"mdp" => $user->mdp,
 			"level" => $user->level));
 	}
 	public function updateMail($request, $response){
 		$user = User::find(1);/* A remplacer par l'id  dans la session*/
-		return $this->view->render($response, 'profil/profilUpdate.twig', array(
+		if (isset($_SESSION['errorMail'])) {
+			echo $_SESSION['errorMail'];
+		}
+		session_destroy();
+		return $this->view->render($response, 'profil/profilUpdateMail.twig', array(
 			"id" => $user->id, 
 			"nom" => $user->nom,
 			"prenom" => $user->prenom,
 			"mail" => $user->mail,
+			"dateNaissance" => $user->dateNaissance,
 			"mdp" => $user->mdp,
 			"level" => $user->level));
 	}
-	public function check($request, $response){
-		var_dump($_POST);
+	public function checkPass($request, $response){
 		if ($_POST['newPass'] != $_POST['newPass2']) {
-			return $response->withRedirect($this->router->pathFor('profilUpdatePass'));
+			$_SESSION['errorPass'] = 'Les mot de passe saisie ne correspondent pas';
+			return $response->withRedirect($this->router->pathFor('updatePass'));
 		}
-		else{
-			return $response->withRedirect($this->router->pathFor('profil'));
+		elseif (strlen($_POST['newPass']) < 8){
+			$_SESSION['errorPass'] = 'Le mot de passe est trop court (8 caractères)';
+			return $response->withRedirect($this->router->pathFor('updatePass'));
+		}
+		elseif ($_POST['newPass'] == $_POST['currentPass']){
+			$_SESSION['errorPass'] = 'Le mot de passe ne doit pas être identique a l\'ancien';
+			return $response->withRedirect($this->router->pathFor('updatePass'));
+		}
+		else{ 
+			$passHash = password_hash ($_POST['newPass'] , PASSWORD_DEFAULT);
+			$user = User::find(1);/* A remplacer par l'id  dans la session*/
+			if (password_verify ( $_POST['currentPass'] , $user->mdp )) {
+				echo "ok";
+				$user->mdp = $passHash;
+				$user->save();
+				return $response->withRedirect($this->router->pathFor('profil'));
+			}
+			else
+			{
+				$_SESSION['errorPass'] = 'L\'ancien mot de passe ne correspond pas avec la base de données.';
+				return $response->withRedirect($this->router->pathFor('updatePass'));
+			}
 		}
 
+	}
+	public function checkMail($request, $response){	
+		if ($_POST['mail'] != $_POST['mail2']) {
+			$_SESSION['errorMail']='diff';
+			return $response->withRedirect($this->router->pathFor('updateMail'));
+		}
+		elseif(!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)){
+			$_SESSION['errorMail']='non valide';
+			return $response->withRedirect($this->router->pathFor('updateMail'));
+		}else{
+			$user = User::find(1);/* A remplacer par l'id  dans la session*/
+			$user->mail = $_POST['mail'];
+			$user->save();
+			return $response->withRedirect($this->router->pathFor('profil'));
+		}
 	}
 }
--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,7 @@
 $app->get("/home", "HomeController:index");
 $app->get("/auth/signup", "AuthController:getSignUp")->setName("auth.signup");
 $app->get("/profil", "ProfilController:index")->setName("profil");
-$app->get("/profil/update", "ProfilController:update");
-$app->get("/profil/updateMail", "ProfilController:updateMail");
-$app->get("/profil/updatePass", "ProfilController:updatePass")->setName("profilUpdatePass");
-$app->post("/profil/updatePass/check", "ProfilController:check");
+$app->get("/profil/updatePass", "ProfilController:updatePass")->setName("updatePass");
+$app->post("/profil/checkPass", "ProfilController:checkPass")->setName("checkPass");
+$app->get("/profil/updateMail", "ProfilController:updateMail")->setName("updateMail");
+$app->post("/profil/checkMail", "ProfilController:checkMail")->setName("checkMail");
