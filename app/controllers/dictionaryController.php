55e809fc99abba2420b2ea6e01d05e7e2cad2590
Louis Maillard <l.maillard55000@gmail.com>
2019-03-03 22:16:49 +0100

    création xml/csv/json + dl

--- app/controllers/dictionaryController.php
+++ app/controllers/dictionaryController.php
@@ -2,6 +2,9 @@
 namespace App\Controllers;
 use \Slim\Views\Twig as View;
 use \App\Models\Dictionary;
+use \App\Models\Sequence;
+use \App\Models\Commentary;
+
 class DictionaryController extends Controller{
 	public function index($request, $response){
 		$methods = Dictionary::where("type", "method")->get();
@@ -11,7 +14,8 @@ class DictionaryController extends Controller{
 
 	public function getById($request, $response, $args){
 		$element = Dictionary::find($args["id"]);
-		return $this->view->render($response, "dictionary/details.twig", array("element" => $element));
+		$sequences = Sequence::where("pseudocode", "like", $args["id"].";%")->orWhere("pseudocode", "like", "%;".$args["id"].";%")->orWhere("pseudocode", "like", "%;".$args["id"])->get();
+		return $this->view->render($response, "dictionary/details.twig", array("element" => $element, "sequences" => $sequences));
 	}
 
 	public function new($request, $response, $args){
@@ -27,5 +31,91 @@ class DictionaryController extends Controller{
 		}
 		$element->save();
 		return $response->withRedirect($this->router->pathFor('dictionary'));
+	}
+
+	public function viewExport($request, $response, $args){
+		return $this->view->render($response, "dictionary/export.twig");
+	}
+
+	public function export($request, $response, $args){
+		$data = array();
+		$sequences = Sequence::select(["id", "pseudocode"])->get();
+		foreach($sequences as $sequence){
+			$pseudocode = explode(";", $sequence->pseudocode);
+			$s = "";
+			for($i = 0; $i < count($pseudocode); $i++){
+				$element = Dictionary::where("id", "=", $pseudocode[$i])->first();
+				if($i > 2){
+					$s .= ", ";
+				}
+				$s .= $element->libelle;
+				if($i == 0){
+					$s .=  ".";
+				}
+				if($i == 1){
+					$s .= "(";
+				}
+			}
+			$s .= ")";
+			$commentaires = Commentary::where("idSequence", "=", $sequence->id)->get();
+			$data[$s] = array();
+			foreach ($commentaires as $commentaire){
+				array_push($data[$s], $commentaire->commentaire);
+			}
+		}
+		$method = $args["format"];
+		$name = "test";
+		$this->$method($data, $name);
+		header('Content-disposition: attachment; filename="'.$name.'.'.$args["format"].'"');
+		header('Content-Type: application/force-download');
+		header('Content-Transfer-Encoding: binary');
+		header('Content-Length: '. filesize('../ressources/export/'.$name.'.'.$args["format"]));
+		header('Pragma: no-cache');
+		header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+		header('Expires: 0');
+		readfile('../ressources/export/'.$name.'.'.$args["format"]);
+	}
+
+	private function xml($data, $name){
+		$xml = new \XMLWriter();
+		$xml->openMemory();
+		$xml->startDocument('2.0', 'utf-8');
+		$xml->startElement("data");	
+		foreach($data as $key => $value){
+			$xml->startElement("sequence");			
+			$xml->writeAttribute("pseudocode", $key);
+			foreach ($value as $commentaire){
+				$xml->writeElement('commentaire', $commentaire);
+			}
+			$xml->endElement();
+		}
+		$xml->endElement();
+		$xml->endDocument();
+		$file = fopen("./../ressources/export/".$name.".xml", "w+");
+		fwrite($file, $xml->flush());
+		fclose($file);
+	}
+
+	private function csv($data, $name){
+		$file = fopen("./../ressources/export/".$name.".csv", "w+");
+		fprintf($file, chr(0xEF).chr(0xBB).chr(0xBF));
+		fputcsv($file, array("sequence", "commentaires"), ";");
+		foreach($data as $key => $value){
+			for($i = 0; $i < count($value); $i++){
+				if($i == 0){
+					fputcsv($file, array($key, $value[$i]), ";");
+				}
+				else{
+					fputcsv($file, array("", $value[$i]), ";");
+				}
+			}
+		}
+		fclose($file);
+	}
+
+	private function json($data, $name){
+		$file = fopen("./../ressources/export/".$name.".json", "w+");
+		fwrite($file, json_encode($data));
+		fclose($file);
 	}
 }
