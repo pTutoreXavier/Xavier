ad7fdda6c4fabac8ead90ce33ef961c7c1ceb340
yvann <yvann.om@orange.fr>
2019-03-25 22:51:31 +0100

    modification création séquence

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -78,7 +78,7 @@ $app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
 $app->add(new \App\Middleware\OldInputMiddleware($container));
 $app->add(new \App\Middleware\CsrfViewMiddleware($container));
 
-$app->add($container->csrf);
+//$app->add($container->csrf);
 
 //REGLES DE VALIDATION
 v::with('App\\Validation\\Rules\\');
